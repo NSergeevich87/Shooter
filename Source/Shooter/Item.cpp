507b6383bb6110ad5fd2ae78a214c05710194dc1
NSergeevich87 <niknikitaser@gmail.com>
2023-11-22 20:26:09 +0100

    Actor_Item_Weapon

---
+++ Source/Shooter/Item.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Item.h"
+#include "Components/BoxComponent.h"
+
+// Sets default values
+AItem::AItem()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	ItemSkeletalMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("ItemSkeletalMesh"));
+	SetRootComponent(ItemSkeletalMesh);
+
+	ItemCollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("ItemCollisionBox"));
+	ItemCollisionBox->SetupAttachment(ItemSkeletalMesh);
+}
+
+// Called when the game starts or when spawned
+void AItem::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AItem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
